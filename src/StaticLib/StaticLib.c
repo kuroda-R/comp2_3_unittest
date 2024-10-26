52c35b66e4a79cb7b0e3f7181a6794b188323d7a
kuroda-R <kuroryu5525@gmail.com>
2024-10-27 00:34:55 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -21,5 +21,14 @@ int sum(int num, int* array)
 // num個の整数の平均を得る
 int average(int num, int* array)
 {
-	return 0x7fffffff;// ■未実装
+	int a = 0;
+
+	for (int i = 0; i < num; i++)
+	{
+		a += array[i];
+	}
+
+	a = a / num;
+
+	return a;// ■未実装
 }
